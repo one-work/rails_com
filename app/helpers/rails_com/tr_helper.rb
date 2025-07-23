316b75ec34613d0bd8bdc9dd9edbf8738159fb2c
qinmingyuan <mingyuan0715@foxmail.com>
2025-07-23 11:56:07 +0800

    more simple override

--- app/helpers/rails_com/tr_helper.rb
+++ app/helpers/rails_com/tr_helper.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
 module RailsCom::TrHelper
 
-  def tr_tag(model, data: {})
+  def tr_tag(model, data: {}, &block)
     data ||= {}
     data.with_defaults!(
       controller: 'show',
       action: 'mouseenter->show#show mouseleave->show#hide'
     )
-    tag.tr(id: "tr_#{model.id}", data: data)
+    tag.tr(id: "tr_#{model.id}", data: data, &block)
+  end
+
+  def tr_actions(model)
+    button_to({ action: 'show', id: model.id }, aria: { label: t('.show.title') }, class: 'button is-small is-rounded is-light') do
+      '<i class="fa-solid fa-circle-info text-info"></i>'.html_safe
+    end
   end
 
 end
