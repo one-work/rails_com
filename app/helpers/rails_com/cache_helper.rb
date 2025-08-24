302e16043ec3b5620aceef233be49643513018cd
qinmingyuan <mingyuan0715@foxmail.com>
2025-08-24 22:51:22 +0800

    simple assets

--- app/helpers/rails_com/cache_helper.rb
+++ app/helpers/rails_com/cache_helper.rb
@@ -2,7 +2,7 @@ module RailsCom
   module CacheHelper
 
     def cache_with_model(namespace = '')
-      ->(model) { [model, namespace, current_member.cache, asset_path('icons_regular.svg')] }
+      ->(model) { [model, namespace, current_member.cache, compute_asset_path('icons_regular.svg')] }
     end
 
   end
