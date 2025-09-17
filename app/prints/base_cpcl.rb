bd5b7ce9a919d779c641feecf73026c7f3f5b5e2
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-17 15:54:52 +0800

    rename method

--- app/prints/base_cpcl.rb
+++ app/prints/base_cpcl.rb
@@ -116,7 +116,7 @@ class BaseCpcl
   end
 
   # todo
-  def line(x0: 0, y0: 36, x1: 40 * 8, y1: 36, width: 2, line_add: true)
+  def line_x(x0: 0, y0: 36, x1: 40 * 8, y1: 36, width: 2, line_add: true)
     @texts << "L #{x0} #{@current_y} #{x1} #{@current_y} #{width}"
     @current_y += y1 if line_add
   end
