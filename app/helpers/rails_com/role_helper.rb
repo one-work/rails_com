f0bd03c050318f89933e49494cbd4938db6d65f7
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-11 16:28:12 +0800

    simple debug

--- app/helpers/rails_com/role_helper.rb
+++ app/helpers/rails_com/role_helper.rb
@@ -63,8 +63,8 @@ module RailsCom::RoleHelper
     result = organ_permitted && user_permitted
     if Rails.configuration.x.role_debug || !result
       logger.debug "\e[35m  Meta Params: #{meta_params}, Extra Params: #{extra_params}  \e[0m"
-      logger.debug "\e[35m  #{current_organ&.base_class_name}_#{current_organ&.id}: #{organ_permitted.inspect}  \e[0m" if defined?(current_organ)
-      logger.debug "\e[35m  #{rails_role_user&.base_class_name}_#{rails_role_user&.id}: #{user_permitted.inspect}  \e[0m"
+      logger.debug "\e[35m  Organ: #{current_organ&.base_class_name}, ID: #{current_organ&.id}: #{organ_permitted.inspect}  \e[0m" if defined?(current_organ)
+      logger.debug "\e[35m  Role User: #{rails_role_user&.base_class_name}, ID: #{rails_role_user&.id}: #{user_permitted.inspect}  \e[0m"
     end
     result
   end
