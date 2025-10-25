c9d65aab58f3c1f405e2b2db95de526ee7ad6a58
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-25 19:46:55 +0800

    add support caceh

--- app/models/com/ext/taxon_prepend.rb
+++ app/models/com/ext/taxon_prepend.rb
@@ -1,5 +1,20 @@
 module Com
   module Ext::TaxonPrepend
+    extend ActiveSupport::Concern
+
+    included do
+      if table_exists? && column_names.include?('children_count')
+        belongs_to(
+          :parent, nil,
+          class_name: _ct.model_class.to_s,
+          foreign_key: _ct.parent_column_name,
+          inverse_of: :children,
+          touch: _ct.options[:touch],
+          counter_cache: :children_count,
+          optional: true
+        )
+      end
+    end
 
     def self_and_siblings
       if self.class.column_names.include?('organ_id')
