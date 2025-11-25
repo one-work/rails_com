589edcb596ef807524e70626f3e164b3d5284938
qinmingyuan <mingyuan0715@foxmail.com>
2025-11-26 00:40:36 +0800

    fix esc

--- app/prints/base_esc.rb
+++ app/prints/base_esc.rb
@@ -97,9 +97,10 @@ class BaseEsc
   end
 
   def text_big_center(data)
-    data_push 0x0d, 0x0a, 0x1b, 0x61, 0x01
+    data_push 0x1b, 0x61, 0x01
     data_push 0x1d, 0x21, 0x11 # Quad area text
     data_push *data.encode('gb18030').bytes
+    data_push 0x0a
     data_push *TXT_NORMAL
     data_push *TXT_ALIGN_LT
     data_push *CTL_LF
