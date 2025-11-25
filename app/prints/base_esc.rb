e1fdb7a3cb73fb74460b274b60241b8ff964874c
qinmingyuan <mingyuan0715@foxmail.com>
2025-11-25 22:00:44 +0800

    alwyasy return self

--- app/prints/base_esc.rb
+++ app/prints/base_esc.rb
@@ -26,20 +26,9 @@ class BaseEsc
 
   # Text formating
   TXT_NORMAL = [ 0x1b, 0x21, 0x00 ]        # Normal text
-  TXT_2HEIGHT = [ 0x1b, 0x21, 0x10 ]        # Double height text
-  TXT_2WIDTH = [ 0x1b, 0x21, 0x20 ]        # Double width text
   TXT_UNDERL_OFF = [ 0x1b, 0x2d, 0x00 ]        # Underline font OFF
-  TXT_UNDERL_ON = [ 0x1b, 0x2d, 0x01 ]        # Underline font 1
-  TXT_UNDERL2_ON = [ 0x1b, 0x2d, 0x02 ]        # Underline font 2
   TXT_BOLD_OFF = [ 0x1b, 0x45, 0x00 ]        # Bold font OFF
-  TXT_BOLD_ON = [ 0x1b, 0x45, 0x01 ]        # Bold font ON
-  TXT_FONT_A = [ 0x1b, 0x4d, 0x00 ]        # Font type A
-  TXT_FONT_B = [ 0x1b, 0x4d, 0x01 ]        # Font type B
   TXT_ALIGN_LT = [ 0x1b, 0x61, 0x00 ]        # 左对齐
-  TXT_ALIGN_CT = [ 0x1b, 0x24, 0x80, 0x00 ]        # 居中对齐
-  TXT_ALIGN_RT = [ 0x1b, 0x61, 0x02 ]        # 右对齐
-  TXT_INVERT_ON = [ 0x1d, 0x42, 0x01 ]        # Inverted color text
-  TXT_INVERT_OFF = [ 0x1d, 0x42, 0x00 ]        # Inverted color text
   TXT_COLOR_BLACK = [ 0x1b, 0x72, 0x00 ]        # Default Color
   TXT_COLOR_RED = [ 0x1b, 0x72, 0x01 ]        # Alternative Color (Usually Red)
 
@@ -50,30 +39,30 @@ class BaseEsc
   attr_reader :data
   def initialize
     @data = []
-    @data.push(0x1b, 0x40)  # 初始化打印机：清除打印缓存，各参数恢复默认值
+    data_push 0x1b, 0x40  # 初始化打印机：清除打印缓存，各参数恢复默认值
     #@data.push(0x1b, 0x4c) # 页模式
-    @data.push(0x1d, 0x4c, 0x12, 0x00)  # 设置左限（左边距）：向右移动 18（0x12）点
+    data_push 0x1d, 0x4c, 0x12, 0x00  # 设置左限（左边距）：向右移动 18（0x12）点
   end
 
   def set_cn
-    @data.push(0x1c, 0x26) # 启用 16×16 点阵中文打印模式
-    @data.push(0x1c, 0x21, 0x00)  # 中文字间距为 0 点
+    data_push 0x1c, 0x26 # 启用 16×16 点阵中文打印模式
+    data_push 0x1c, 0x21, 0x00  # 中文字间距为 0 点
   end
 
   def partial_cut!
-    @data.concat(PAPER_PARTIAL_CUT)
+    data_push *PAPER_PARTIAL_CUT
   end
 
   def cut!
-    @data.concat(PAPER_FULL_CUT)
+    data_push *PAPER_FULL_CUT
   end
 
   def break_line
-    @data.concat(CTL_LF)
+    data_push *CTL_LF
   end
 
   def render
-    @data.push(*(CTL_LF * 10))
+    data_push *(CTL_LF * 10)
     cut!
   end
 
@@ -86,61 +75,62 @@ class BaseEsc
   end
 
   def text(data)
-    @data.push *TXT_NORMAL
-    @data.push *data.encode('gb18030').bytes
-    @data.push *TXT_NORMAL
-    @data.push *CTL_LF
+    data_push *TXT_NORMAL
+    data_push *data.encode('gb18030').bytes
+    data_push *TXT_NORMAL
+    data_push *CTL_LF
   end
 
   def double_height(data)
-    @data.concat *[TXT_2HEIGHT, data.bytes, TXT_NORMAL]
+    data_push 0x1b, 0x21, 0x10, *data.bytes, *TXT_NORMAL
   end
 
   def text_big(data)
-    @data.push 0x1b, 0x21, 0x30 # Quad area text
-    @data.push *data.encode('gb18030').bytes
-    @data.push *TXT_NORMAL
-    @data.push *CTL_LF
+    data_push 0x1b, 0x21, 0x30 # Quad area text
+    data_push *data.encode('gb18030').bytes
+    data_push *TXT_NORMAL
+    data_push *CTL_LF
   end
 
   def double_width(data)
-    @data.concat *[TXT_2WIDTH, data.bytes, TXT_NORMAL]
+    data_push 0x1b, 0x21, 0x20, *data.bytes, *TXT_NORMAL
   end
 
   def underline(data)
-    @data.concat *[TXT_UNDERL_ON, data.bytes, TXT_UNDERL_OFF]
+    data_push 0x1b, 0x2d, 0x01, *data.bytes, *TXT_UNDERL_OFF
   end
 
   def underline2(data)
-    @data.concat *[TXT_UNDERL2_ON, data.bytes, TXT_UNDERL_OFF]
+    data_push 0x1b, 0x2d, 0x02, *data.bytes, *TXT_UNDERL_OFF
   end
 
   def bold(data)
-    @data.concat *[TXT_BOLD_ON, data.bytes, TXT_BOLD_OFF]
+    data_push 0x1b, 0x45, 0x01, *data.bytes, TXT_BOLD_OFF
   end
 
   def left(data = '')
-    @data.concat *[TXT_ALIGN_LT, data.bytes, TXT_ALIGN_LT]
+    data_push *TXT_ALIGN_LT, data.bytes, *TXT_ALIGN_LT
   end
 
   def right(data = '')
-    @data.concat *[TXT_ALIGN_RT, data.bytes, TXT_ALIGN_LT]
+    data_push 0x1b, 0x61, 0x02, *data.bytes, *TXT_ALIGN_LT
   end
 
   def center(data = '')
-    @data.concat *[TXT_ALIGN_CT, data.encode('gb18030').bytes, TXT_ALIGN_LT]
+    data_push 0x1b, 0x24, 0x80, 0x00, *data.bytes, *TXT_ALIGN_LT
   end
 
+  # Inverted color text
   def inverted(data)
-    @data.concat *[TXT_INVERT_ON, data.bytes, TXT_INVERT_OFF]
+    data_push 0x1d, 0x42, 0x01, *data.bytes, 0x1d, 0x42, 0x00
   end
 
   def black
-    @data.concat *[TXT_COLOR_BLACK, data.bytes, TXT_COLOR_BLACK]
+    data_push *TXT_COLOR_BLACK, *data.bytes, *TXT_COLOR_BLACK
   end
 
   def red
-    @data.concat *[TXT_COLOR_BLACK, data.bytes, TXT_COLOR_RED]
+    data_push *TXT_COLOR_BLACK, *data.bytes, *TXT_COLOR_RED
   end
 
   def qrcode(data, y: nil)
@@ -151,14 +141,8 @@ class BaseEsc
     qr_data = [0x1d, 0x28, 0x6b, bytes.size + 3, 0x00, 0x31, 0x50, 0x30] # 二维码数据
     qr_run = [0x1d, 0x28, 0x6b, 0x03, 0x00, 0x31, 0x51, 0x30] # 打印二维码
 
-    @data.concat *[
-      qr_size,
-      qr_err,
-      qr_data,
-      bytes,
-      qr_run
-    ]
-    @data.concat(CTL_LF * 2)
+    data_push *qr_size, *qr_err, *qr_data, *bytes, *qr_run
+    data_push *(CTL_LF * 2)
   end
 
   # 0 不显示数据，只显示条码
@@ -181,22 +165,20 @@ class BaseEsc
       raise ArgumentError("Width must be in range from 2 to 6.")
     end
 
-    @data.concat *[
-      text_position,
-      bar_height,
-      bar_width,
-      bar_format,
-      data.bytes,
-      [0x00]
-    ]
+    data_push *text_position, *bar_height, *bar_width, *bar_format, *data.bytes, 0x00
   end
 
   def partial_cut
-    @data.concat(PAPER_PARTIAL_CUT)
+    data_push *PAPER_PARTIAL_CUT
   end
 
   def cut
-    @data.push(0x1d, 0x56, 0x00)  # 全切纸
+    data_push 0x1d, 0x56, 0x00  # 全切纸
+  end
+
+  def data_push(*data)
+    @data.concat data
+    self
   end
 
 end
