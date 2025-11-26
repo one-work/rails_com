b26ada0556b81b64a9051db8e922e16f2c1ff033
qinmingyuan <mingyuan0715@foxmail.com>
2025-11-26 22:08:58 +0800

    reset rolescahse

--- app/models/roled/model/role.rb
+++ app/models/roled/model/role.rb
@@ -8,7 +8,7 @@ module Roled
       attribute :description, :string
       attribute :visible, :boolean, default: false
       attribute :role_hash, :json, default: {}
-      attribute :default, :boolean
+      attribute :default, :boolean, default: false
 
       belongs_to :organ, class_name: 'Org::Organ', optional: true
 
@@ -30,13 +30,17 @@ module Roled
 
       validates :name, presence: true
 
-      after_update :set_default, if: -> { default? && saved_change_to_default? }
       after_save :sync, if: -> { saved_change_to_role_hash? }
       after_save :reset_cache!, if: -> { saved_change_to_role_hash? }
+      after_save :reset_all_cache!, if: -> { saved_change_to_default? }
     end
 
     def reset_cache!
       caches.find_each { |i| i.reset_role_hash! }
+    end
+
+    def reset_all_cache!
+      caches.where(who_type: role_types.pluck(:who_type)).find_each { |i| i.reset_role_hash! }
     end
 
     def has_role?(business: nil, namespace: nil, controller: nil, action: nil, **params)
@@ -53,10 +57,6 @@ module Roled
       logger.debug "\e[35m business: #{business}, namespace: #{namespace}, controller: #{controller}, action: #{action}, params: #{params} \e[0m"
     ensure
       0
-    end
-
-    def set_default
-      self.class.where.not(id: self.id).update_all(default: false)
     end
 
     def role_types_hash
