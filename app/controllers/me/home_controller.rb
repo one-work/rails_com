02b64a154fbc17bd4ebc5f123b30809b5143322d
qinmingyuan <mingyuan0715@foxmail.com>
2025-12-20 22:40:16 +0800

    fixlogic

--- app/controllers/me/home_controller.rb
+++ app/controllers/me/home_controller.rb
@@ -6,25 +6,6 @@ module Me
 
     def index
       set_roled_tabs
-
-      @share = {
-        url: 'https://one.work/factory/productions',
-        title: '官方商场'
-      }
-
-      if current_organ
-        @organ_tabs = current_organ.tabs.where(namespace: '').pluck(:path)
-
-        @share.merge!(
-          url: url_for(controller: 'factory/productions', host: current_organ.host, only_path: false),
-          title: current_organ.name,
-          debug: current_organ.debug_enabled
-        )
-
-        if current_organ.share_logo.attached?
-          @share.merge!(share_logo: current_organ.share_logo_url)
-        end
-      end
     end
 
     private
