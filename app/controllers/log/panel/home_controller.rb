16db873bbacb3eceee8bbb2d130d88c5252b89e0
qinmingyuan <mingyuan0715@foxmail.com>
2026-01-01 14:22:51 +0800

    just show total > 10

--- app/controllers/log/panel/home_controller.rb
+++ app/controllers/log/panel/home_controller.rb
@@ -4,7 +4,7 @@ module Log
     def index
       @data = SummaryDaily.where(date: Date.today.prev_month..).select(:date, :duration_avg).order(date: :asc).as_json(only: ['date', 'duration_avg'])
 
-      @request_dailies = RequestDaily.where(date: Date.today.prev_day).order(duration_avg: :desc).limit(10)
+      @request_dailies = RequestDaily.where(date: Date.today.prev_day, total: 10..).order(duration_avg: :desc).limit(10)
     end
 
   end
