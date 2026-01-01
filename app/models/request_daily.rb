1e9b713c8d16bbd19d949dde09b18f11bbea5ab0
qinmingyuan <mingyuan0715@foxmail.com>
2026-01-01 14:19:53 +0800

    show all logic

---
+++ app/models/request_daily.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+RequestDaily = Log::RequestDaily unless defined? RequestDaily
---
+++ app/models/summary_daily.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+SummaryDaily = Log::SummaryDaily unless defined? SummaryDaily
