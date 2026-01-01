2ea3d41c1f5cbcba1e5e4b7a858ba6a5deda9cdd
qinmingyuan <mingyuan0715@foxmail.com>
2026-01-01 20:11:24 +0800

    queue

--- app/models/job/model/queue.rb
+++ app/models/job/model/queue.rb
@@ -4,9 +4,9 @@ module Job
 
     included do
       scope :todo, -> { scheduled.where(scheduled_at: Time.current..) }
-      scope :blocked, -> { associated(:blocked_execution) }
-      scope :running, -> { associated(:claimed_execution) }
-      scope :ready, -> { associated(:ready_execution) }
+      scope :blocked, -> { where.associated(:blocked_execution) }
+      scope :running, -> { where.associated(:claimed_execution) }
+      scope :ready, -> { where.associated(:ready_execution) }
     end
 
   end
