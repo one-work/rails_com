88719f696420db69751c887e7a89fa769f8eef91
qinmingyuan <tomq@everymarket.com>
2026-01-05 10:04:10 +0800

    only use diffferent connects in test env

--- app/models/doc/base_record.rb
+++ app/models/doc/base_record.rb
@@ -1,6 +1,8 @@
 module Doc
   class BaseRecord < ActiveRecord::Base
     self.abstract_class = true
-    connects_to database: { writing: :prod }
+    if Rails.env.test?
+      connects_to database: { writing: :prod }
+    end
   end
 end
