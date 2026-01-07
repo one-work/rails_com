db9d0077165bbc5a4be564116ac07184915eeb7b
qinmingyuan <mingyuan0715@foxmail.com>
2026-01-07 15:07:53 +0800

    subject

--- app/controllers/doc/subjects_controller.rb
+++ app/controllers/doc/subjects_controller.rb
@@ -1,9 +1,14 @@
 module Doc
   class SubjectsController < BaseController
+    before_action :set_subject, only: [:show, :edit, :update, :destroy]
 
     def index
       @subjects = Subject.page(params[:page])
     end
 
+    private
+    def set_subject
+      @subject = Subject.find params[:id]
+    end
   end
 end
