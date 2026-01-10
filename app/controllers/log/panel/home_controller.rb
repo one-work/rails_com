0052de1987a0c0cfd7eaa3b33e57bada59ba1a2c
qinmingyuan <mingyuan0715@foxmail.com>
2026-01-10 20:39:21 +0800

    doc

--- app/controllers/log/panel/home_controller.rb
+++ app/controllers/log/panel/home_controller.rb
@@ -3,11 +3,15 @@ module Log
 
     def index
       yesterday = Date.yesterday
-
       @data = SummaryDaily.where(date: yesterday.prev_month..).select(:date, :duration_avg).order(date: :asc).as_json(only: ['date', 'duration_avg'])
 
-      @request_dailies = RequestDaily.where(date: yesterday, total: 10..).order(duration_avg: :desc).limit(10)
-      @query_dailies = QueryDaily.where(date: yesterday, total: 10..).order(duration_avg: :desc).limit(10)
+      @request_dailies = RequestDaily.where(date: yesterday).order(duration_avg: :desc)
+      request_min = @request_dailies.minimum(:total)
+      @request_dailies = @request_dailies.where(total: request_min..).limit(10)
+
+      @query_dailies = QueryDaily.where(date: yesterday, total: 10..).order(duration_avg: :desc)
+      query_min = @query_dailies.minimum(:total)
+      @query_dailies = @query_dailies.where(total: query_min..).limit(10)
     end
 
   end
--- app/models/doc/model/subject.rb
+++ app/models/doc/model/subject.rb
@@ -52,14 +52,14 @@ module Doc
       end
 
       def docs_hash
-        if Rails.root.join('config/doc.yml').exist?
-          doc_hash = YAML.load_file(Rails.root.join('config/doc.yml'))
+        if Rails.root.join('docs/response.yml').exist?
+          doc_hash = YAML.load_file(Rails.root.join('docs/response.yml'))
         else
           doc_hash = {}
         end
 
         Rails::Engine.subclasses.each_with_object(doc_hash) do |engine, h|
-          doc_path = engine.root.join('config/doc.yml')
+          doc_path = engine.root.join('docs/response.yml')
           if doc_path.exist?
             YAML.safe_load_file(doc_path).each do |k, v|
               h[k] ||= {}
