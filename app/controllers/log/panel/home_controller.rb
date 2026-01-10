d5f82899c2314fddc81b4ebfe40b792574192f22
qinmingyuan <mingyuan0715@foxmail.com>
2026-01-10 20:43:55 +0800

    limit

--- app/controllers/log/panel/home_controller.rb
+++ app/controllers/log/panel/home_controller.rb
@@ -6,11 +6,11 @@ module Log
       @data = SummaryDaily.where(date: yesterday.prev_month..).select(:date, :duration_avg).order(date: :asc).as_json(only: ['date', 'duration_avg'])
 
       @request_dailies = RequestDaily.where(date: yesterday).order(duration_avg: :desc)
-      request_min = @request_dailies.minimum(:total)
+      request_min = @request_dailies.limit(10).minimum(:total)
       @request_dailies = @request_dailies.where(total: request_min..).limit(10)
 
-      @query_dailies = QueryDaily.where(date: yesterday, total: 10..).order(duration_avg: :desc)
-      query_min = @query_dailies.minimum(:total)
+      @query_dailies = QueryDaily.where(date: yesterday).order(duration_avg: :desc)
+      query_min = @query_dailies.limit(10).minimum(:total)
       @query_dailies = @query_dailies.where(total: query_min..).limit(10)
     end
 
