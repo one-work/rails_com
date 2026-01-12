b133ccaf5c0a9791818fa8a8f461ca7a1799f385
qinmingyuan <mingyuan0715@foxmail.com>
2026-01-12 10:18:51 +0800

    tr actions

--- app/helpers/rails_com/tr_helper.rb
+++ app/helpers/rails_com/tr_helper.rb
@@ -1,19 +1,12 @@
 # frozen_string_literal: true
 module RailsCom::TrHelper
 
-  def tr_tag(model, data: {}, &block)
-    data ||= {}
-    data.with_defaults!(
-      controller: 'show',
-      action: 'mouseenter->show#show mouseleave->show#hide'
-    )
-    tag.tr(id: "tr_#{model.id}", data: data, &block)
-  end
-
-  def tr_actions(model)
-    button_to({ action: 'show', id: model.id }, aria: { label: t('.show.title') }, class: 'button is-small is-borderless') do
-      svg_tag 'circle-info', class: 'icon text-info'
-    end
+  def tr_actions(model, icons: ['show', 'edit', 'destroy'])
+    [
+      { title: t('.show.title'), icon: 'eye', action: url_for(action: 'show', id: model.id) },
+      { title: t('.edit.title'), icon: 'pencil', action: url_for(action: 'edit', id: model.id) },
+      { title: t('.destroy.title'), icon: 'trash', method: 'destroy', action: url_for(action: 'destroy', id: model.id) }
+    ].to_json
   end
 
 end
