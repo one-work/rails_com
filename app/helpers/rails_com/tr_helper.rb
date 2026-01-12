fd4e7c3062afd316a47df0395960c418f63dfa0f
qinmingyuan <mingyuan0715@foxmail.com>
2026-01-12 13:18:17 +0800

    logic with hlepr

--- app/helpers/rails_com/tr_helper.rb
+++ app/helpers/rails_com/tr_helper.rb
@@ -1,12 +1,22 @@
 # frozen_string_literal: true
 module RailsCom::TrHelper
 
-  def tr_actions(model, icons: ['show', 'edit', 'destroy'])
-    [
-      { title: t('.show.title'), icon: 'eye', action: url_for(action: 'show', id: model.id) },
-      { title: t('.edit.title'), icon: 'pencil', action: url_for(action: 'edit', id: model.id) },
-      { title: t('.destroy.title'), icon: 'trash', method: 'destroy', action: url_for(action: 'destroy', id: model.id) }
-    ].to_json
+  def tr_actions(model, *items, icons: ['show', 'edit', 'destroy'])
+    defaults = []
+
+    if icons.include?('show')
+      defaults << { title: t('.show.title'), icon: 'eye', action: url_for(action: 'show', id: model.id) }
+    end
+
+    if icons.include?('edit')
+      defaults << { title: t('.edit.title'), icon: 'pencil', action: url_for(action: 'edit', id: model.id) }
+    end
+
+    if icons.include?('destroy')
+      defaults << { title: t('.destroy.title'), icon: 'trash', method: 'destroy', action: url_for(action: 'destroy', id: model.id) }
+    end
+
+    (defaults + items).to_json
   end
 
 end
