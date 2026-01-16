329c382349ff811bab64d0d7ae3e608e3bc79352
qinmingyuan <mingyuan0715@foxmail.com>
2026-01-16 17:01:23 +0800

    cache logic with houst

--- app/helpers/rails_com/cache_helper.rb
+++ app/helpers/rails_com/cache_helper.rb
@@ -2,11 +2,15 @@ module RailsCom
   module CacheHelper
 
     def cache_with_model(namespace = '')
-      ->(model) { [model, namespace, current_member.cache, compute_asset_path('icons_regular.svg')] }
-    end
-
-    def cache_with_svg(&block)
-      cache([compute_asset_path('icons_regular.svg')], {}, &block)
+      ->(model) do
+        [
+          model,
+          namespace,
+          current_member.cache,
+          compute_asset_path('icons_regular.svg'),
+          compute_asset_host('icons_regular.svg')
+        ]
+      end
     end
 
   end
