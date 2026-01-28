efe89c91e09c164190075d10e3ccf3b6f4427945
qinmingyuan <mingyuan0715@foxmail.com>
2026-01-28 22:05:10 +0800

    api controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,4 @@
 class ApiController < ActionController::API
-  include RailsCom::Application
-  include Auth::Controller::Application if defined? RailsAuth
-
   protect_from_forgery with: :exception, unless: -> { json_format? }
 
 end
