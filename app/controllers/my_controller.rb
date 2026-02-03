07d1d899c55b05d81e2f8d256b83816f9122aa15
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-03 15:16:19 +0800

    fix error

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,5 +1,5 @@
 class MyController < ApplicationController
-  include Com::Controller::Admin
+  include Com::Controller::My
   include Trade::Controller::My if defined? RailsTrade
   before_action :require_user
 
