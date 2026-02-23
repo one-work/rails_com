f6edc75bc5af4e5f4b272a844458f73ec55a5029
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-23 20:49:52 +0800

    distance plan

--- lib/rails_com/utils_module/time_util.rb
+++ lib/rails_com/utils_module/time_util.rb
@@ -85,4 +85,25 @@ module TimeUtil
     seconds
   end
 
+  def distance_date(from = Date.today, to, **options)
+    result = (to.to_date - from.to_date).to_i
+
+    options = {
+      scope: 'date.distance_in_words'
+    }.merge!(options)
+
+    I18n.with_options locale: options[:locale], scope: options[:scope] do |locale|
+      case result
+      when 0
+        locale.t :zero
+      when 1
+        locale.t :one
+      when 2
+        locale.t :two
+      else
+        locale.t :other, count: result
+      end
+    end
+  end
+
 end
