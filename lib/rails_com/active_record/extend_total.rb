4409e3ae0606632b89043118e9870d39bbfac21a
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-25 11:09:07 +0800

    threshold logic

--- lib/rails_com/active_record/extend_total.rb
+++ lib/rails_com/active_record/extend_total.rb
@@ -4,10 +4,10 @@ module RailsCom::ActiveRecord
   module ExtendTotal
 
     def total_threshold(threshold, column:, order: 'expire_at')
-      with(running_total: self.select(:id, column, Arel.sql("SUM(#{column}) OVER (ORDER BY #{order} DESC) AS total")))
-        .from(:running_total)
-        .where(Arel.sql("total <= :threshold OR (total > :threshold AND total - #{column} < :threshold)"), threshold: threshold)
-        .order(column => :desc)
+      with(running_total: self.select('id', column, order, Arel.sql("SUM(#{column}) OVER (ORDER BY #{order} DESC) AS total")))
+        .from('running_total')
+        .select('id', column, order)
+        .where(total: threshold..)
     end
 
   end
