a19c6ff08f9d6ca37136f0d81e27df5c4a06c92f
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-25 15:47:44 +0800

    code style

--- lib/rails_com/active_record/extend_total.rb
+++ lib/rails_com/active_record/extend_total.rb
@@ -7,7 +7,8 @@ module RailsCom::ActiveRecord
       with(running_total: self.select('id', column, order, Arel.sql("SUM(#{column}) OVER (ORDER BY #{order} ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS total")))
         .from('running_total')
         .select('id', column, order, 'total')
-      .unscope(:where).where(Arel.sql("total < :threshold + #{column}"), threshold: threshold)
+        .unscope(:where)
+        .where(Arel.sql("total < :threshold + #{column}"), threshold: threshold)
     end
 
   end
