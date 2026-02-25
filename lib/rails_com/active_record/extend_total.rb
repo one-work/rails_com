fdfb215bf577911dea24ecc35ef81af2056936e7
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-25 15:31:05 +0800

    simplelist

--- lib/rails_com/active_record/extend_total.rb
+++ lib/rails_com/active_record/extend_total.rb
@@ -4,10 +4,10 @@ module RailsCom::ActiveRecord
   module ExtendTotal
 
     def total_threshold(threshold, column:, order: 'expire_at')
-      with(running_total: self.select('id', column, order, Arel.sql("SUM(#{column}) OVER (ORDER BY #{order} ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING) AS total")))
+      with(running_total: self.select('id', column, order, Arel.sql("SUM(#{column}) OVER (ORDER BY #{order} ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS total")))
         .from('running_total')
-        .select('id', column, order)
-        .where(Arel.sql("total < :threshold"), threshold: threshold)
+        .select('id', column, order, 'total')
+        .where(Arel.sql("total < :threshold + #{column}"), threshold: threshold)
     end
 
   end
