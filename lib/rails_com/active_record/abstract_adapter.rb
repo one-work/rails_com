6dc5deec543844f0ed5bbdf2d52c5699b7116eee
qinmingyuan <mingyuan0715@foxmail.com>
2026-03-31 14:36:46 +0800

    clear teask

--- lib/rails_com/active_record/abstract_adapter.rb
+++ lib/rails_com/active_record/abstract_adapter.rb
@@ -4,8 +4,28 @@ module RailsCom::ActiveRecord
   module AbstractAdapter
 
     def truncate_tables(*table_names)
-      table_names -= Meta::BaseRecord.subclasses.map(&:table_name)
-      table_names -= Doc::BaseRecord.subclasses.map(&:table_name)
+      table_names -= [
+        'meta_actions',
+        'meta_businesses',
+        'meta_columns',
+        'meta_controllers',
+        'meta_namespaces',
+        'meta_operations',
+        'meta_records',
+        'doc_maps',
+        'doc_subjects',
+        'solid_queue_blocked_executions',
+        'solid_queue_claimed_executions',
+        'solid_queue_failed_executions',
+        'solid_queue_jobs',
+        'solid_queue_pauses',
+        'solid_queue_processes',
+        'solid_queue_ready_executions',
+        'solid_queue_recurring_executions',
+        'solid_queue_recurring_tasks',
+        'solid_queue_scheduled_executions',
+        'solid_queue_semaphores'
+      ]
 
       super
     end
