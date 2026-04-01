31b651cab3bef6251508f084faa46acb0ec8963e
qinmingyuan <mingyuan0715@foxmail.com>
2026-04-01 16:25:17 +0800

    fix cache

--- app/helpers/rails_com/cache_helper.rb
+++ app/helpers/rails_com/cache_helper.rb
@@ -13,5 +13,17 @@ module RailsCom
       end
     end
 
+    def cache_with_organ(namespace = '')
+      ->(model) do
+        [
+          model,
+          namespace,
+          current_member.cache,
+          current_member.organ,
+          compute_asset_path('icons_regular.svg')
+        ]
+      end
+    end
+
   end
 end
