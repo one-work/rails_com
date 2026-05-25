778f0094c7f390918173f8f31a2c869475f830e5
qinmingyuan <mingyuan0715@foxmail.com>
2026-05-25 22:29:55 +0800

    lnt

--- app/models/com/ext/geo.rb
+++ app/models/com/ext/geo.rb
@@ -9,7 +9,7 @@ module Com
     class_methods do
 
       def near(lnt, lng, column: :geo)
-        pt = RGeo::Geos.factory(srid: 4326).point(lnt, lng)
+        pt = RGeo::Geos.factory(srid: 4326).point(lnt.to_f, lng.to_f)
         select(Arel.star, arel_table[column].st_distance(Arel.spatial(pt)).as('distance')).order(distance: :asc)
       end
 
