bb8f5bf80bc752dbf98c52f46d804930fd3239e4
qinmingyuan <mingyuan0715@foxmail.com>
2026-05-25 14:19:38 +0800

    fix

--- lib/rails_com/core/date_format.rb
+++ lib/rails_com/core/date_format.rb
@@ -1,14 +1,8 @@
 # frozen_string_literal: true
 
-Date::DATE_FORMATS[:month_and_day] = '%m-%d'
-Date::DATE_FORMATS[:year_and_month] = '%Y-%m'
-Date::DATE_FORMATS[:week] = ->(date) {
-  I18n.t('date.day_names')[date.wday]
-}
-Date::DATE_FORMATS[:month] = ->(date) {
-  I18n.t('date.month_names')[date.month]
-}
-Date::DATE_FORMATS[:year_month] = '%Y年%m月'
-Date::DATE_FORMATS[:month_day] = ->(date) {
-  "#{date.month}月#{date.mday}日"
-}
+ActiveSupport::DateFormats.register(:month_and_day, '%m-%d')
+ActiveSupport::DateFormats.register(:year_and_month, '%Y-%m')
+ActiveSupport::DateFormats.register(:week, ->(date) { I18n.t('date.day_names')[date.wday] })
+ActiveSupport::DateFormats.register(:month, ->(date) { I18n.t('date.month_names')[date.month] })
+ActiveSupport::DateFormats.register(:year_month, '%Y年%m月')
+ActiveSupport::DateFormats.register(:month_day, ->(date) { "#{date.month}月#{date.mday}日" })
--- lib/rails_com/core/time_format.rb
+++ lib/rails_com/core/time_format.rb
@@ -1,28 +1,24 @@
 # frozen_string_literal: true
 
-Time::DATE_FORMATS[:human] = '%Y-%m-%d %H:%M:%S'
-Time::DATE_FORMATS[:datetime] = '%Y-%m-%d %H:%M'
-Time::DATE_FORMATS[:local] = '%Y-%m-%dT%H:%M:%S'
-Time::DATE_FORMATS[:with_usec] = '%Y-%m-%d %H:%M:%S.%6N'
-Time::DATE_FORMATS[:wechat] = '%Y年%-m月%-d日 %H:%M'
-Time::DATE_FORMATS[:short_cn] = '%-m月%-d日 %H:%M'
-Time::DATE_FORMATS[:hour] = '%H'
-Time::DATE_FORMATS[:minute] = '%M'
-Time::DATE_FORMATS[:week] = ->(time) {
-  I18n.t('date.day_names')[time.wday]
-}
-Time::DATE_FORMATS[:date] = ->(time) {
-  t = Time.zone.at time
-  t.strftime '%Y-%m-%d'
-}
-Time::DATE_FORMATS[:month] = ->(time) {
-  I18n.t('date.month_names')[time.month]
-}
-Time::DATE_FORMATS[:shorter] = ->(time) {
-  t = Time.zone.at time
-  if t.to_date == Date.today
-    t.strftime '%H:%M'
-  else
-    t.strftime '%m-%d'
-  end
-}
+ActiveSupport::TimeFormats.register(:human, '%Y-%m-%d %H:%M:%S')
+ActiveSupport::TimeFormats.register(:datetime, '%Y-%m-%d %H:%M')
+ActiveSupport::TimeFormats.register(:local, '%Y-%m-%dT%H:%M:%S')
+ActiveSupport::TimeFormats.register(:with_usec, '%Y-%m-%d %H:%M:%S.%6N')
+ActiveSupport::TimeFormats.register(:wechat, '%Y年%-m月%-d日 %H:%M')
+ActiveSupport::TimeFormats.register(:short_cn, '%-m月%-d日 %H:%M')
+ActiveSupport::TimeFormats.register(:hour, '%H')
+ActiveSupport::TimeFormats.register(:minute, '%M')
+ActiveSupport::TimeFormats.register(:week, ->(time) { I18n.t('date.day_names')[time.wday] })
+ActiveSupport::TimeFormats.register(:date, ->(time) { Time.zone.at(time).strftime('%Y-%m-%d') })
+ActiveSupport::TimeFormats.register(:month, ->(time) { I18n.t('date.month_names')[time.month] })
+ActiveSupport::TimeFormats.register(
+  :shorter,
+  ->(time) {
+    t = Time.zone.at(time)
+    if t.to_date == Date.today
+      t.strftime '%H:%M'
+    else
+      t.strftime '%m-%d'
+    end
+  }
+)
