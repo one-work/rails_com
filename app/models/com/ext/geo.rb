aeae6bac9f10008b5dec4a8e4a140c77fdc581f4
qinmingyuan <mingyuan0715@foxmail.com>
2026-05-25 23:03:44 +0800

    distance

--- app/models/com/ext/geo.rb
+++ app/models/com/ext/geo.rb
@@ -3,12 +3,17 @@ module Com
     extend ActiveSupport::Concern
 
     included do
-      attr_reader :distance
       attribute :geo, :st_point, srid: 4326, geographic: true
     end
 
     def set_geo!(lng, lat)
       update geo: RGeo::Geos.factory(srid: 4326).point(lng, lat)
+    end
+
+    def distance
+      if defined? super
+        super
+      end
     end
 
     class_methods do
