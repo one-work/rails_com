761d9460c4726f532f92ee807b53e6fe528fe555
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-17 14:47:53 +0800

    for debug

--- app/helpers/rails_com/role_helper.rb
+++ app/helpers/rails_com/role_helper.rb
@@ -47,6 +47,7 @@ module RailsCom::RoleHelper
     end
 
     if filtered && defined?(current_organ) && current_organ
+      logger.debug "========#{current_organ}"
       organ_permitted = current_organ.has_role?(params: extra_params, **meta_params)
     else
       organ_permitted = true
