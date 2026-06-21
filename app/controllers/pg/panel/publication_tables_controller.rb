21595aa49163e5796d890cd57a526b0e2bb888a1
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-21 13:15:46 +0800

    allt ables

--- app/controllers/pg/panel/publication_tables_controller.rb
+++ app/controllers/pg/panel/publication_tables_controller.rb
@@ -1,19 +1,14 @@
 module Pg
   class Panel::PublicationTablesController < Panel::BaseController
-    before_action :set_publication
 
     def index
       q_params = {
         'tablename-ll': params[:tablename]
       }
 
-      @publication_tables = @publication.publication_tables.default_where(q_params).page(params[:page])
-    end
-
-    private
-    def set_publication
       BaseRecord.connected_to(**shard_params) do
         @publication = Publication.find params[:publication_id]
+        @publication_tables = @publication.publication_tables.default_where(q_params).page(params[:page])
       end
     end
 
