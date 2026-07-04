6079284381b77512adae3c74b93d626eab7870a6
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-04 22:41:29 +0800

    support set default role by hash

--- app/models/roled/model/role.rb
+++ app/models/roled/model/role.rb
@@ -228,5 +228,27 @@ module Roled
       end
     end
 
+    class_methods do
+
+      def default_role_hash
+        if Rails.root.join('config/roles.yml').exist?
+          init_hash = YAML.load_file(Rails.root.join('config/roles.yml'))
+        else
+          init_hash = {}
+        end
+
+        Rails::Engine.subclasses.each_with_object(init_hash) do |engine, h|
+          icon_path = engine.root.join('config/roles.yml')
+          if icon_path.exist?
+            YAML.safe_load_file(icon_path).each do |k, v|
+              h[k] ||= []
+              h[k].concat v
+            end
+          end
+        end
+      end
+
+    end
+
   end
 end
