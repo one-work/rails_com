002c948a73e7b3a377ddd3771a5d8af6bec51fdc
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-06 22:31:21 +0800

    seuprt

--- app/controllers/roled/panel/caches_controller.rb
+++ app/controllers/roled/panel/caches_controller.rb
@@ -3,12 +3,21 @@ module Roled
     before_action :set_cache, only: [:show, :edit, :update, :destroy]
 
     def index
-      @caches = Cache.order(id: :desc).page(params[:page])
+      q_params = {}
+      q_params.merge! params.permit(:id)
+
+      @caches = Cache.where(q_params).order(id: :desc).page(params[:page])
     end
 
     private
     def set_cache
       @cach = Cache.find params[:id]
+    end
+
+    def set_filter_columns
+      @filter_columns = set_filter_i18n(
+        'id' => { type: 'string', default: true }
+      )
     end
 
   end
