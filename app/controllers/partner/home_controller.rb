c5c6b9989778ecd7036a2556b869017fc0a984a8
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-21 16:17:13 +0800

    partner

--- app/controllers/partner/home_controller.rb
+++ app/controllers/partner/home_controller.rb
@@ -3,6 +3,31 @@ module Partner
     skip_before_action :require_role, only: [:index] if whether_filter :require_role
 
     def index
+      set_roled_tabs
+      @share = {
+        url: 'https://one.work/factory/productions',
+        title: '官方商场',
+        appId: 'wxe44dc002dd0d29b0'
+      }
+
+      if current_organ
+        if request.variant.include? :mini_program
+          host = current_organ.mp_host
+        else
+          host = current_organ.host
+        end
+        @organ_tabs = current_organ.tabs.where(namespace: '').pluck(:path)
+
+        @share.merge!(
+          title: current_organ.name,
+          debug: current_organ.debug_enabled,
+          url: url_for(controller: 'org/organs', host: host)
+        )
+
+        if current_organ.share_logo.attached?
+          @share.merge!(share_logo: current_organ.share_logo_url)
+        end
+      end
     end
 
   end
