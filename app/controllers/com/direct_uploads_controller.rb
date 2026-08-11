8bf9767f02041baeb8815031472917d5c2791760
qinmingyuan <mingyuan0715@foxmail.com>
2026-08-11 16:54:12 +0800

    fix

--- app/controllers/com/direct_uploads_controller.rb
+++ app/controllers/com/direct_uploads_controller.rb
@@ -1,7 +1,7 @@
 module Com
   class DirectUploadsController < ActiveStorage::DirectUploadsController
     if whether_filter :verify_authenticity_token
-      skip_before_action :verify_authenticity_token, only: [:create]
+      skip_forgery_protection only: [:create]
     end
 
     # change_column_null :active_storage_blobs, :checksum, true
