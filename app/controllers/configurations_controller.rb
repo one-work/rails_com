541548a8e8cfdf0718e43280c060df526219c43e
qinmingyuan <mingyuan0715@foxmail.com>
2026-08-25 16:26:49 +0800

    fixed

--- app/controllers/configurations_controller.rb
+++ app/controllers/configurations_controller.rb
@@ -10,7 +10,7 @@ class ConfigurationsController < ApplicationController
           },
           {
             id: 'init',
-            url: 'https://assets.linlishenghuo.com/assets/printer-00000098.digested.js'
+            url: 'https://assets.linlishenghuo.com/assets/printer-00000099.digested.js'
           }
         ]
       },
@@ -65,7 +65,7 @@ class ConfigurationsController < ApplicationController
           },
           {
             id: 'init',
-            url: 'https://assets.linlishenghuo.com/assets/printer-00000098.digested.js'
+            url: 'https://assets.linlishenghuo.com/assets/printer-00000099.digested.js'
           }
         ]
       },
