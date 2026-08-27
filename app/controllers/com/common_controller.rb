103c1b111be40ea90dcfc39692cfb4c25bcd373d
qinmingyuan <mingyuan0715@foxmail.com>
2026-08-27 22:59:03 +0800

    geo logic;

--- app/controllers/com/common_controller.rb
+++ app/controllers/com/common_controller.rb
@@ -89,7 +89,6 @@ module Com
 
     def geo
       @url = URI(params[:url])
-      @url.query = { latitude: params[:latitude] }.to_query
       session[:latitude] = params[:latitude]
       session[:longitude] = params[:longitude]
     end
