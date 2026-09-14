7e8937a31cc8166f84b55f0edd4c6234255fe2eb
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-14 12:49:26 +0800

    note set roleevia

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,6 +1,5 @@
 module Admin
   class HomeController < BaseController
-    skip_before_action :require_role, only: [:index] if whether_filter :require_role
 
     def index
       @businesses = Meta::Business.includes(:admin_actions).where.not(identifier: ['', 'design'])
